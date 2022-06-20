14a849645a1da583ebc98f0719ab3658385315cd
Karim Mohamed <komicho1996@gmail.com>
2022-06-21 01:59:02 +0200

    Feat: create booking component

---
+++ app/Http/Livewire/Booking.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Bus;
+use App\Models\Station;
+use App\Models\Ticket;
+use App\Models\User;
+use Livewire\Component;
+use Jantinnerezo\LivewireAlert\LivewireAlert;
+
+class Booking extends Component
+{
+    use LivewireAlert;
+
+    public $stationsTarget = null;
+    public $stationsCount = null;
+    public $totalCountForTickets = null;
+    public $seatsAvailable = null;
+
+    public $bus_id;
+    public $user_id;
+    public $station_from_id;
+    public $station_to_id;
+
+    public function departureStation()
+    {
+        $this->stationsTarget = Station::where('id', '>', $this->station_from_id)->get();
+    }
+
+    public function arrivalStation()
+    {
+        $this->stationsCount = Station::where('id', '>', $this->station_from_id)
+            ->where('id', '<=', $this->station_to_id)
+            ->count();
+
+        $seats_not_available = Ticket::where('bus_id', '=', $this->bus_id)
+            ->where('station_to_id', '>=', $this->station_from_id)
+            ->where('station_from_id', '<=', $this->station_to_id)
+            ->count();
+
+        $this->seatsAvailable = $seats_not_available;
+
+        // dd($this->seatsAvailable);
+    }
+
+    public function book()
+    {
+        if ($this->seatsAvailable >= 12) {
+            return $this->alert('warning', 'No seats available.');
+        }
+
+        $ticket = Ticket::create([
+            'user_id' => $this->user_id,
+            'bus_id' => $this->bus_id,
+            'station_from_id' => $this->station_from_id,
+            'station_to_id' => $this->station_to_id,
+        ]);
+
+        $this->alert('success', 'Seat reserved ticket number is: ' . $ticket->id);
+    }
+
+    public function render()
+    {
+        $buses = Bus::get();
+        $users = User::get();
+        $stations = Station::get();
+
+        return view('livewire.booking', [
+            'buses' => $buses,
+            'users' => $users,
+            'stations' => $stations,
+            'stations_target' => $this->stationsTarget,
+            'stations_count' => $this->stationsCount,
+            'seats_available_count' => $this->seatsAvailable,
+        ]);
+    }
+}
---
+++ resources/views/livewire/booking.blade.php
@@ -0,0 +1,67 @@
+<div>
+    <form wire:submit.prevent="book">
+
+        <div class="mt-4">
+            <label class="block font-medium text-sm text-gray-700">Bus:</label>
+
+            <select wire:model="user_id" class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+                <option value=>Select bus</option>
+                
+                @foreach ($buses as $bus)
+                    <option value="{{ $bus->id }}">{{ $bus->vehicle_id }}</option>
+                @endforeach
+            </select>
+        </div>
+
+        <div class="mt-4">
+            <label class="block font-medium text-sm text-gray-700">User:</label>
+
+            <select wire:model="bus_id" class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+                <option value=>Select user</option>
+                
+                @foreach ($users as $user)
+                    <option value="{{ $user->id }}">{{ $user->name }}</option>
+                @endforeach
+            </select>
+        </div>
+
+        <div class="mt-4">
+            <label class="block font-medium text-sm text-gray-700">Station from:</label>
+
+            <select wire:model="station_from_id" wire:change="departureStation"
+                class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+                <option value=>Select station</option>
+
+                @foreach ($stations as $station)
+                    <option value="{{ $station->id }}">{{ $station->name }}</option>
+                @endforeach
+            </select>
+        </div>
+
+        <div class="mt-4">
+            <label class="block font-medium text-sm text-gray-700">Station to:</label>
+
+            <select wire:model="station_to_id" wire:change="arrivalStation"
+                class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+                <option value=>Select station</option>
+
+                @if ($stations_target)
+                    @foreach ($stations_target as $station)
+                        <option value="{{ $station->id }}">{{ $station->name }}</option>
+                    @endforeach
+                @endif
+            </select>
+        </div>
+
+        <div class="flex items-center justify-end mt-4">
+            @if ($stations_count)
+                <span class="text-sm text-gray-600 hover:text-gray-900">
+                    <p>The number of stations between the selected stations: {{ $stations_count }}</p>
+                    <p>Number of seats available: {{ $seats_available_count }}</p>
+                </span>
+            @endif
+            <x-button class="ml-3">Book</x-button>
+        </div>
+
+    </form>
+</div>
