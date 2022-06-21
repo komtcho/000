bcf3a6f064ed1ca8dfa84bf250deb911b752d97b
Karim Mohamed <komicho1996@gmail.com>
2022-06-21 03:56:59 +0200

    Test: create a feature test for booking

---
+++ tests/Feature/Api/BookingTest.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace Tests\Feature\Api;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class BookingTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        // seed the database
+        $this->artisan('db:seed');
+    }
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_booking()
+    {
+        $user = User::factory()->create();
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        $response = $this->withHeaders([
+            'Accept' => 'application/json',
+            'authorization' => 'Bearer ' . $token,
+        ])->post('/api/book', [
+            'bus_id' => 1,
+            'station_from_id' => 1,
+            'station_to_id' => 6,
+        ]);
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_booking_not_authorize()
+    {
+        $response = $this->withHeaders([
+            'Accept' => 'application/json',
+        ])->post('/api/book', [
+            'bus_id' => 1,
+            'station_from_id' => 1,
+            'station_to_id' => 6,
+        ]);
+
+        $response->assertStatus(401);
+    }
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_booking_select_station_not_available()
+    {
+        $user = User::factory()->create();
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        $response = $this->withHeaders([
+            'Accept' => 'application/json',
+            'authorization' => 'Bearer ' . $token,
+        ])->post('/api/book', [
+            'bus_id' => 1,
+            'station_from_id' => 1,
+            'station_to_id' => 7,
+        ]);
+
+        $response->assertStatus(422);
+    }
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_booking_number_seats_exceeded_maximum()
+    {
+        $user = User::factory()->create();
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        \App\Models\Ticket::factory(12)
+            ->sequence(
+                [
+                    'user_id' => 1,
+                    'bus_id' => 1,
+                    'station_from_id' => 1,
+                    'station_to_id' => 6,
+                ],
+            )
+            ->create();
+
+        $response = $this->withHeaders([
+            'Accept' => 'application/json',
+            'authorization' => 'Bearer ' . $token,
+        ])->post('/api/book', [
+            'bus_id' => 1,
+            'station_from_id' => 1,
+            'station_to_id' => 6,
+        ]);
+
+        $response->assertStatus(400);
+    }
+}
