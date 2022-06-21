7e04bf9cae4234a7398b7d1c66841f747ad39187
Karim Mohamed <komicho1996@gmail.com>
2022-06-21 08:28:07 +0200

    Refactor: remove `next_station_id` from stations table

---
+++ database/migrations/2022_06_21_062544_remove_next_station_id_from_stations.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('stations', function (Blueprint $table) {
+            $table->dropColumn('next_station_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('stations', function (Blueprint $table) {
+            $table->integer('next_station_id')->unique()->nullable();
+        });
+    }
+};
